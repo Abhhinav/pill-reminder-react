5086e5cf9383bb9b75e903e3677985288a8f97a8
abhhinav <abhinavkmr86@gmail.com>
2020-10-01 17:40:26 +0530

    loginpage and fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Link, NavLink, BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import CurrentUserChecker from './components/current-user-checker';
+import NavBar from './components/navbar';
+import MainApp from './features/mainapp';
+import Register from './features/register';
+import AuthenticatedRoutes from './components/authenticated-routes';
+import Profile from './features/profile';
+import MedicalHistory from './features/medical-history';
 import './App.css';
+import {CurrentUserProvider} from './providers/current-user-provider';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="container-fluid">
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+
+        <Router>
+          <NavBar title="PILLS REMINDER" />
+          <Switch>
+            <Route path='/' exact>
+              <div className="d-flex mt-5 justify-content-center align-items-center">
+                <MainApp />
+              </div>
+            </Route>
+            
+            <Route path="/register">
+              <div 
+                className="d-flex mt-5 justify-content-center align-items-center">
+                <Register />
+              </div>
+            </Route>
+            
+            <AuthenticatedRoutes>
+              <Route path="/profile">
+                <div 
+                  className="d-flex mt-5 justify-content-center align-items-center">
+                  <Profile />
+                </div>
+              </Route>
+              <Route path="/medical-history">
+                <div 
+                  className="d-flex mt-5 justify-content-center align-items-center">
+                  <MedicalHistory />
+                </div>
+              </Route>
+            </AuthenticatedRoutes>
+          </Switch>
+        </Router>
+       </CurrentUserChecker>
+     </CurrentUserProvider>
     </div>
-  );
+   )
 }
 
-export default App;
---
+++ src/constants/api_constants.js
@@ -0,0 +1,5 @@
+export const API_SESSIONS = "http://localhost:3001/sessions";
+
+export const Constant = {
+  AUTH_TOKEN: "MEM_AUTH_TOKEN"
+}
